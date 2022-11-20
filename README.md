d602802092c1f612d727ad7ea79baaf465263e70
Minh Phat <phat.caominh.dev@gmail.com>
2022-11-20 12:02:24 +0700

    Sqlalchemy to postgresql

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # flask-note-app
 A project to learning concept of flask
+
+#Setup & Installtion:
+pip install -r requirements.txt
+
+Install postgresql and pgadmin
+
+Set up path for database in __init__ file
+
+#Running The App:
+python main.py
+
+#Viewing The App:
+Go to http://127.0.0.1:5000
--- website/__init__.py
+++ website/__init__.py
@@ -2,24 +2,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import psycopg2
 
-db = SQLAlchemy()
 DB_NAME = "database.db"
-
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:243278@localhost/FlaskNoteApp'
+db = SQLAlchemy(app) 
+db.init_app(app)
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
-
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
+    from .models import User
 
     create_database(app)
 
